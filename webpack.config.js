8d1bc3e13127288fd344aa711718ffce4f0b2e7d
shashank dewangan <shashank.at30@gmail.com>
2021-06-11 14:51:09 +0530

    added less loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+  ],
   devServer: {
     contentBase: './dist',
   },
